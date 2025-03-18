4df553902b613bf9ad1edc0f2e2e60bb1c95297c
vaneyepes <48944459+vaneyepes@users.noreply.github.com>
2025-03-18 16:59:44 +0100

    Updated API integration
    bugs fixed

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ function displayPoem(response) {
 function genratePoem(event) {
   event.preventDefault();
 
-  topicInput = document.querySelector("#topic-input");
+  let topicInput = document.querySelector("#topic");
   let apiKey = "3b0ad82f5b38c08bad26a6o21848t64c";
-  let prompt =
-    "Please generate a poem using the word provided in ${#topicInput.value}";
+  let prompt = `Please generate a poem using the word provided in ${topicInput.value}`;
   let context =
-    "You are a robot who is in love with a microwave. The robot is trying to impress the microwave by cooking a delicious meal. please make it short and funny. space and brake lines in a poem style. Generate a new poem every time the user clicks the button.";
+    "You are a robot who is in love with a microwave. The robot is trying to impress the microwave by cooking a delicious meal. Make it funny. Your goal is to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not show the word html in your response. Generate a new poem every time the user clicks the button. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
